4508971469230f1d7abe3a8e813470c28fa2df90
u64lisa <Nils-Schrock@web.de>
2023-11-20 19:14:17 +0100

    Added File loading from resources

--- src/main/java/axiol/Main.java
+++ src/main/java/axiol/Main.java
@@ -2,6 +2,9 @@ package axiol;
 
 import axiol.lexer.Lexer;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,10 +13,20 @@ public class Main {
         lexer.addTokenRule("NUMBER", "\\d+");
         lexer.addTokenRule("IDENTIFIER", "[a-zA-Z_]+");
         lexer.addTokenRule("PLUS", "\\+");
-        lexer.addTokenRule("PLUS", "\\-");
+        lexer.addTokenRule("MINUS", "\\-");
+        lexer.addTokenRule("EQUALS", "\\=");
         lexer.addTokenRule("WHITESPACE", "\\s+");
 
-        System.out.println(lexer.tokenize("test + 12 - OwO"));
+        System.out.println(lexer.tokenize(readFile("/test/syntax.ax")));
+    }
 
+    public static final String readFile(String file) {
+        Scanner scanner = new Scanner(Objects.requireNonNull(Main.class.getResourceAsStream(file)));
+        StringBuilder fileContents = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            fileContents.append(scanner.nextLine()).append("\n");
+        }
+        scanner.close();
+        return fileContents.toString();
     }
 }
