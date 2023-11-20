f71ed2d8ed808163af33636e400748b0685a5040
u64lisa <Nils-Schrock@web.de>
2023-11-20 19:32:24 +0100

    Created RegexBased Lexer Rule

---
+++ src/main/java/axiol/lexer/LexerRule.java
@@ -0,0 +1,66 @@
+package axiol.lexer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class LexerRule {
+    private static final String ESCAPED_PATTERN = "%s.*?%s";
+    private static final String UNESCAPED_PATTERN = "%s(?:%s(?:%s|%s|(?!%s).)|(?!%s|%s).)*%s";
+
+    private int head;
+    private List<Pattern> patterns;
+
+    public LexerRule(int head) {
+        this.head = head;
+
+        this.patterns = new ArrayList<>();
+    }
+
+    private LexerRule addDelimiter(String open, String escape, String close, int flags) {
+        String startingSymbol = regexEscape(open);
+        String closingSymbol = regexEscape(close);
+        String escapeRegex = regexEscape(escape);
+
+        String regex = escape.isEmpty() ? ESCAPED_PATTERN.formatted(
+                startingSymbol, closingSymbol
+        ) :
+                UNESCAPED_PATTERN.formatted(
+                        startingSymbol, escapeRegex, escapeRegex, closingSymbol,
+                        closingSymbol, escapeRegex, closingSymbol, closingSymbol
+                );
+
+        this.patterns.add(Pattern.compile(regex, flags));
+        return this;
+    }
+
+    String regexEscape(String string) {
+        if (string == null) return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+
+            switch (c) {
+                case '\0', '\n', '\r', '\t', '\\', '^', '$', '?', '|', '*', '/', '+', '.', '(', ')', '[', ']', '{', '}':
+                    sb.append('\\').append(c);
+                    continue;
+            }
+
+            if (c > 0xff) sb.append("\\u").append(String.format("%0" + 4 + "x", c));
+            else if (Character.isISOControl(c)) sb.append("\\x").append(String.format("%0" + 2 + "x", c));
+            else sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    public List<Pattern> getPatterns() {
+        return patterns;
+    }
+
+    public int getHead() {
+        return head;
+    }
+}
