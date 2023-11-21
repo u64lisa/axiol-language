e52d7ff7422e07b804a87875bcf57e0bd4ea9ef4
u64lisa <Nils-Schrock@web.de>
2023-11-21 17:03:23 +0100

    Finished Basic number expressions (Binary, Unary)

--- src/main/java/axiol/parser/ExpressionParser.java
+++ src/main/java/axiol/parser/ExpressionParser.java
@@ -1,12 +1,9 @@
 package axiol.parser;
 
-import axiol.lexer.TokenType;
-import axiol.parser.LanguageParser;
 import axiol.parser.expression.Operator;
 import axiol.parser.tree.Expression;
+import axiol.parser.tree.expressions.BinaryExpression;
 import axiol.parser.tree.expressions.UnaryExpression;
-
-import java.util.List;
 
 public class ExpressionParser {
 
@@ -19,7 +16,14 @@ public class ExpressionParser {
     public Expression parseExpression(int priority) {
         Operator[] operators = Operator.getOperatorsByPriority(priority);
 
+        if (priority == 0) {
+            // atomics
+        }
+
+        int operatorCycles = 0;
         Expression leftAssociated = null;
+
+        // unary
         for (Operator operator : operators) {
             if (!operator.isUnary() || operator.isLeftAssociated() ||
                     this.languageParser.getTokenStream().matches(operator.getType()))
@@ -32,13 +36,45 @@ public class ExpressionParser {
             leftAssociated = this.parseExpression(priority - 1);
         }
 
-        return null;
-    }
+        // append last lef-associated Expression
+        while (operatorCycles == 0) {
+            operatorCycles = 1;
 
-    // binary
-    // unary
-    // num
-    // string
-    // call
+            for (Operator operator : operators) {
+                if (!operator.isUnary() || !operator.isLeftAssociated() ||
+                        this.languageParser.getTokenStream().matches(operator.getType()))
+                    continue;
+
+                languageParser.getTokenStream().advance();
+                operatorCycles = 0;
+
+                leftAssociated = new UnaryExpression(operator, leftAssociated);
+            }
+        }
+        // reset for next loop
+        operatorCycles = 0;
+
+        // binary expression from past unary left
+        while (operatorCycles == 0) {
+            operatorCycles = 1;
+
+            for (Operator operator : operators) {
+                if (operator.isUnary() || languageParser.getTokenStream().matches(operator.getType())) {
+                    continue;
+                }
+
+                languageParser.getTokenStream().advance();
+                operatorCycles = 0;
+
+                Expression right = operator.isLeftAssociated() ?
+                        parseExpression(priority - 1) : parseExpression(priority);
+
+                leftAssociated = new BinaryExpression(operator, leftAssociated, right);
+            }
+        }
+
+
+        return leftAssociated;
+    }
 
 }
