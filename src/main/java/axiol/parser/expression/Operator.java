adf4a0fc22dee17cda2ea05de107c81f27e20bef
u64lisa <Nils-Schrock@web.de>
2023-11-21 15:16:39 +0100

    Repaired Enum format

--- src/main/java/axiol/parser/expression/Operator.java
+++ src/main/java/axiol/parser/expression/Operator.java
@@ -2,54 +2,59 @@ package axiol.parser.expression;
 
 import axiol.lexer.TokenType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public enum Operator {
 
     // Basic Assignment
-    ASSIGN("=", TokenType.EQUAL, 1, false, false),
+    ASSIGN(        "=",  TokenType.EQUAL,          1,  false, false),
 
     // Logical Operators
-    AND("&&", TokenType.EQUAL_EQUAL, 3, false, false),
-    OR("||", TokenType.OR_OR, 2, false, false),
+    AND(           "&&", TokenType.EQUAL_EQUAL,    3,  false, false),
+    OR(            "||", TokenType.OR_OR,          2,  false, false),
 
     // Increment and Decrement
-    DECREASE("--", TokenType.MINUS_MINUS, 11, true, false), // unary
-    INCREASE("++", TokenType.PLUS_PLUS, 11, true, false), // unary
+    DECREASE(      "--", TokenType.MINUS_MINUS,    11, true, false), // unary
+    INCREASE(      "++", TokenType.PLUS_PLUS,      11, true, false), // unary
 
     // Basic Arithmetic
-    PLUS("+", TokenType.PLUS, 8, false, false),
-    MINUS("-", TokenType.MINUS, 8, false, false),
-    MULTIPLE("*", TokenType.MULTIPLY, 9, false, false),
-    DIVIDE("/", TokenType.DIVIDE, 9, false, false),
-    MOD("%", TokenType.MOD, 9, false, false),
-    XOR("^", TokenType.XOR, 7, false, false),
-    QUESTION("?", TokenType.QUESTION, 10, false, false),
-    NOT("!", TokenType.QUESTION, 10, true, true), // unary
+    PLUS(          "+",  TokenType.PLUS,           8,  false, false),
+    MINUS(         "-",  TokenType.MINUS,          8,  false, false),
+    MULTIPLE(      "*",  TokenType.MULTIPLY,       9,  false, false),
+    DIVIDE(        "/",  TokenType.DIVIDE,         9,  false, false),
+    MOD(           "%",  TokenType.MOD,            9,  false, false),
+    XOR(           "^",  TokenType.XOR,            7,  false, false),
+    QUESTION(      "?",  TokenType.QUESTION,       10, false, false),
+    NOT(           "!",  TokenType.QUESTION,       10, true,  true), // unary
 
     // Comparison
-    LESS_THAN("<", TokenType.LESS_THAN, 4, false, false),
-    MORE_THAN(">", TokenType.MORE_THAN, 4, false, false),
-    EQUAL_EQUAL("==", TokenType.EQUAL_EQUAL, 5, false, false),
+    LESS_THAN(     "<",  TokenType.LESS_THAN,      4,  false, false),
+    MORE_THAN(     ">",  TokenType.MORE_THAN,      4,  false, false),
+    EQUAL_EQUAL(   "==", TokenType.EQUAL_EQUAL,    5,  false, false),
 
     // Comparison with Assignment
-    LESS_EQUAL("<=", TokenType.LESS_EQUAL, 4, false, false),
-    MORE_EQUAL(">=", TokenType.MORE_EQUAL, 4, false, false),
+    LESS_EQUAL(    "<=", TokenType.LESS_EQUAL,     4,  false, false),
+    MORE_EQUAL(    ">=", TokenType.MORE_EQUAL,     4,  false, false),
 
     // Arithmetic with Assignment
-    MIN_EQUAL("-=", TokenType.MINUS_EQUAL, 12, false, false),
-    MUL_EQUAL("*=", TokenType.MULTIPLY_EQUAL, 12, false, false),
-    DIVIDE_EQUAL("/=", TokenType.DIVIDE_EQUAL, 12, false, false),
-    XOR_EQUAL("^=", TokenType.XOR_EQUAL, 12, false, false),
-    NOR_EQUAL("~=", TokenType.NOR_EQUAL, 12, false, false),
+    MIN_EQUAL(     "-=", TokenType.MINUS_EQUAL,    12, false, false),
+    MUL_EQUAL(     "*=", TokenType.MULTIPLY_EQUAL, 12, false, false),
+    DIVIDE_EQUAL(  "/=", TokenType.DIVIDE_EQUAL,   12, false, false),
+    XOR_EQUAL(     "^=", TokenType.XOR_EQUAL,      12, false, false),
+    NOR_EQUAL(     "~=", TokenType.NOR_EQUAL,      12, false, false),
     QUESTION_EQUAL("?=", TokenType.QUESTION_EQUAL, 12, false, false),
-    OR_EQUAL("|=", TokenType.OR_EQUAL, 12, false, false),
+    OR_EQUAL(      "|=", TokenType.OR_EQUAL,       12, false, false),
 
     // Bitwise and Shift Operators
-    BIT_OR("|", TokenType.OR, 6, false, false),
-    SHIFT_LEFT("<<", TokenType.SHIFT_LEFT, 13, false, false),
-    SHIFT_RIGHT(">>", TokenType.SHIFT_RIGHT, 13, false, false),
-    NOR("~", TokenType.NOR, 7, true, false), // unary
+    BIT_OR(        "|",  TokenType.OR,             6,  false, false),
+    SHIFT_LEFT(    "<<", TokenType.SHIFT_LEFT,     13, false, false),
+    SHIFT_RIGHT(   ">>", TokenType.SHIFT_RIGHT,    13, false, false),
+    NOR(           "~",  TokenType.NOR,            7,  true,  false), // unary
     ;
 
     public static final Operator[] VALUES = Operator.values();
