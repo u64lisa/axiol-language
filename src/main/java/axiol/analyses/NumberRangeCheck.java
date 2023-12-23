dff4faa053586c598a72b158a2aad6ef905c5822
u64lisa <Nils-Schrock@web.de>
2023-12-23 17:44:19 +0100

    Updated RangeCheck

--- src/main/java/axiol/analyses/NumberRangeCheck.java
+++ src/main/java/axiol/analyses/NumberRangeCheck.java
@@ -1,5 +1,6 @@
 package axiol.analyses;
 
+import axiol.types.custom.I128;
 import axiol.types.custom.U128;
 
 public class NumberRangeCheck {
@@ -11,11 +12,11 @@ public class NumberRangeCheck {
     }
 
     public boolean isValid(Number value) {
-        //if (value instanceof I128 i128) {
-
-        //}
+        if (value instanceof I128 i128) {
+            return i128.biggerThan(I128.MIN_VALUE) && i128.smallerThan(I128.MAX_VALUE);
+        }
         if (value instanceof U128 u128) {
-
+            return u128.biggerThan(U128.MIN_VALUE) && u128.smallerThan(U128.MAX_VALUE);
         }
 
         return value.longValue() >= min.longValue() && value.longValue() <= max.longValue();
